8d881804ef6ed89da943b3d20086ed448b4e5925
lv fushun <lvfushun168@outlook.com>
2026-03-30 16:23:28 +0800

    init

---
+++ src/main/java/com/ghq/edgegateway/EdgeGateway.java
@@ -0,0 +1,13 @@
+package com.ghq.edgegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeGateway {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdgeGateway.class, args);
+    }
+
+}
